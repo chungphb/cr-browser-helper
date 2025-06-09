41ad269cb35f8539b5b917a7c8a4525e279d1e71
Phạm Hữu Bảo Chung <phamhuubaochung@gmail.com>
2025-06-09 10:41:12 +0700

    Example: Find all GN files

--- src/extension.ts
+++ src/extension.ts
@@ -1,22 +1,22 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "cr-browser-helper" is now active!');
+	
+	const disposable = vscode.commands.registerCommand('cr-browser-helper.findAllGNFiles', async () => {
+		const files = await vscode.workspace.findFiles('**/*.gn', '');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('cr-browser-helper.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from cr-browser-helper!');
+		if (files.length === 0) {
+			vscode.window.showInformationMessage('No GN files found.');
+      return;
+		}
+
+		const output = vscode.window.createOutputChannel('GN File Finder');
+    output.clear();
+    output.show();
+
+		output.appendLine(`Found ${files.length} GN files:\n`);
+    files.forEach(file => output.appendLine(file.fsPath));
 	});
 
 	context.subscriptions.push(disposable);
